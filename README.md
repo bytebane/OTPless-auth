c3af7d74ef2a83db443fa7024d9b14112c347f64
ByteBane <81875670+bytebane@users.noreply.github.com>
2023-12-13 20:30:24 +0530

    Added links to implementations

--- README.md
+++ README.md
@@ -1,64 +1,16 @@
 # OTPless-auth
 
-> Deployed Website - [Click Here](https://otpless-auth.pages.dev/) or [W3spaces](https://otpless-bane.w3spaces.com/)
-
 ## Implementing [OTPless-auth](https://otpless.com/)
 
 ## What is OTPless-auth?
 
 > OTPLESS is a platform that enables secure, seamless, passwordless, and OTPLESS sign-ins for applications and websites across multiple platforms and frameworks. It supports various sign-in methods like WhatsApp, UPI, Google, Apple, GitHub, Slack, Microsoft, and more.
 
-## How to use OTPless-auth?
+## Example Usage
 
-> This Project uses OTPless-SDK with HTML+CSS+JS Technology.
+- [HTML+CSS+JS](https://github.com/bytebane/OTPless-js-auth)
+- [ReactJS](https://github.com/bytebane/OTPless-react-auth)
+- [Firebase]('https://github.com/bytebane/OTPless-firebase-auth)
 
-> Official Documentation for desired platform can be found [here](https://otpless.com/platforms).
+### Official Documentation for platform specific implementations can be found [here](https://otpless.com/platforms)
 
-### To use OTPless-auth in plain JavaScript
-
-- Add div tag
-
-```HTML
-<!-- Any Container Element should be fine, I am using main here just for semantic purpose  -->
-<div id="otpless-auth"></div>
-```
-
-- Add script tag
-
-```HTML
-<script type="text/javascript" src="https://otpless.com/auth.js" cid="PSNMYE48LDJMC5SN2I4UTOD9S9QP4L4P"></script>
-```
-
-- Add the following function in your script
-
-```js
-/**
- * @param {Object} otplessUser - Received User object from OTPless
- * @returns {void}
-*/
-function otpless(otplessUser) {
-    console.log(otplessUser)
-    // Do something with otplessUser
-    // save it in browser storage or database or send it to server.
-    // ...
-    // Add your success callback here, if you want to do something on successful authentication
-    // ...
-}
-```
-
->**Note:**
->> ***OTPless*** authentication **SDK** seamlessly combines both ***sign-in*** and ***sign-up*** functionalities. In cases where the user is already registered, the system generates a new *Auth-Token* with each sign-in request. This *Auth-Token*, accompanied by the user object, is then sent as a response. Alternatively, for unregistered users, the system creates a new User Account using publicly available data (such as name and email) from the chosen Auth-Provider (e.g., Gmail or WhatsApp). A new *Auth-Token* is then generated and sent as a response, along with the user object.
-
-#### Warning
-
-- DO NOT USE HTTP
-- OTPless-auth needs secure connection to work. Use HTTPS.
-Using insecure connection may lead to unexpected behavior. (e.g. Blank Container for the Auth Form)
-
-#### Solution:
-
-- Either use some online IDE that provides secure connection(e.g. [W3Schools-Spaces](https://spaces.w3schools.com/space/))
-- or use dummy data for development stage,and add OTPless-auth to your project in pre-production/test stage in a secured environment.
-- or simply Containerize your development environment with Nginx+self-signed SSL certificates.
-
-#### TLDR: Use HTTPS
--- index.js
+++ index.js
@@ -1,52 +1,38 @@
+const examples = [
+	{
+		id: 001,
+		name: 'JavaScript Auth',
+		url: 'https://github.com/bytebane/OTPless-js-auth',
+	},
+	{
+		id: 002,
+		name: 'ReactJS Auth',
+		url: 'https://github.com/bytebane/OTPless-react-auth',
+	},
+	{
+		id: 003,
+		name: 'Firebase Auth',
+		url: 'https://github.com/bytebane/OTPless-firebase-auth',
+	},
+	{
+		id: 004,
+		name: 'Firebase+ReactJS Auth',
+		url: 'https://github.com/bytebane/OTPless-firebase-auth/tree/main/firebase-react-auth',
+	},
+]
 document.addEventListener('DOMContentLoaded', () => {
-	//* Get the data from sessionStorage
-	// const data = JSON.parse(sessionStorage.getItem('data'))
-	//* Get the data from localStorage
-	const data = JSON.parse(localStorage.getItem('data'))
+	const appEL = document.getElementById('app')
+	const examplesListEl = document.getElementById('examples-list')
 
-	//? If no data is found, redirect to auth.html
-	if (!data) {
-		alert('No data found! Please login first.')
-		window.location.href = 'auth.html'
-	}
-
-	//? If data is found, log and render the data on the page
-	console.log('User Data: ', data)
-
-	const dataContainerEl = document.getElementById('user-data') //* data-container
-	const greetEl = document.getElementById('greet') //* greeting header
-	const signOutBtn = document.getElementById('sign-out') //* sign-out button
-
-	/**
-	 * * Function to render elements from an object's key-value pairs in the DOM
-	 * * @param {Object} input - The object to render
-	 * * @param {string} prepend - (Optional) The string to prepend to the key
-	 * * @returns {void}
-	 * */
-	const renderElementsfromObj = (input, prepend) =>
-		Object.entries(input).forEach(([key, value]) => {
-			//? Skip the token key from being rendered
-			if (key === 'token') return
-			//? If the value is an object, recursively render its key-value pairs
-			if (typeof value === 'object') renderElementsfromObj(value, key)
-			//? Render the object's key-value pairs in the DOM (append each key-value to data-container)
-			else
-				dataContainerEl.innerHTML += `
-					<div class="key-value">
-						<span class="key">${prepend ? prepend + '-' + key : key}: </span>
-						<span class="value">${value}</span>
-					</div>
-				`
-		})
-
-	//? Render the data on the page
-
-	greetEl.innerText = 'Hello ' + data.waName ?? data.mobile.name ?? data.email.name ?? 'User' + '!' //* Set the greeting header
-	data && renderElementsfromObj(data) //* Render the data on the page
-
-	//* Set the sign-out button action
-	signOutBtn.addEventListener('click', () => {
-		sessionStorage.removeItem('data') //* Remove the data from sessionStorage
-		window.location.href = 'auth.html' //* Redirect to auth.html
+	examples.forEach((example) => {
+		const liEl = document.createElement('li')
+		liEl.className = 'list-item'
+		const aEl = document.createElement('a')
+		aEl.className = 'list-item-link'
+		aEl.href = example.url
+		aEl.target = '_blank'
+		aEl.textContent = example.name
+		liEl.appendChild(aEl)
+		examplesListEl.appendChild(liEl)
 	})
 })
